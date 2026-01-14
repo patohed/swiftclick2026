80c8f939637bacfa59026cc0b4336c2b98d15735
patohed <patohed@example.com>
2026-01-14 17:46:23 -0300

    ENVIAR a contacto@swiftclick.com.ar (TO) usando no-reply como FROM

--- api/send-email.js
+++ api/send-email.js
@@ -44,12 +44,12 @@ export default async function handler(req, res) {
       });
     }
 
-    console.log('📧 Enviando email a info@swiftclick.com.ar...');
+    console.log('📧 Enviando email a contacto@swiftclick.com.ar...');
     
     // Enviar email con Resend
     const data = await resend.emails.send({
       from: 'SwiftClick <no-reply@swiftclick.com.ar>',
-      to: ['info@swiftclick.com.ar'],
+      to: ['contacto@swiftclick.com.ar'],
       reply_to: email,
       subject: `[SwiftClick] Nuevo contacto: ${empresa} - ${nombre}`,
       text: `
